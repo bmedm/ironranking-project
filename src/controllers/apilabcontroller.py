a0a4e6b67ab2b3d4db87ae329f601a5d9b8abfb6
bmedm <b.medialm@hotmail.es>
2020-09-24 19:57:10 +0200

    parte de lab

--- src/controllers/apilabcontroller.py
+++ src/controllers/apilabcontroller.py
@@ -19,3 +19,33 @@ def randommeme(lab_id):
         { "$project" : { "Meme" : 1, "Teacher": 1,"_id":0}}
       ])
     return dumps(res)
+
+
+@app.route("/lab/<labname>/search")
+def allOpenClose(labname):
+    
+    opened_pr = db.pull.find({"$and": [{"Lab":f"{labname}"},{"State": "open"}]}).count()
+    closed_pr = db.pull.find({"$and": [{"Lab":f"{labname}"},{"State": "closed"}]}).count()
+    meme=db.pull.find({"Lab":f"{labname}"}).distinct("Meme")
+    timestamp=db.pull.find({"Lab":f"{labname}"})
+        
+    #db.collection.update( {}, {"$set": {"Correcion":{"$subtract":["$ISODate('Closed')","$ISODate('Update')"]}}}, {"upsert":False}, {"multi":True})
+    #timestamp=db.pull.find({"Lab":f'{labname}'},{"Lab":1,"Correccion":1,"_id":0})
+
+    res = {'-El numero de PR abiertas es:': opened_pr,
+           '-El numero de PR cerradas es:': closed_pr,
+           '-El porcentaje closed es:':f' {round(closed_pr/(opened_pr+closed_pr)*100,2)} %',
+           '-El porcentaje open es:': f'{round(opened_pr/(opened_pr+closed_pr)*100,2)}%',
+           '-La lista de memes únicos es:':meme
+           }
+    return dumps(res)
+
+
+
+
+
+
+
+
+
+
